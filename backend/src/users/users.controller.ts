a26ab11353ebdc5e5b24a365f2462918eccb63b5
Jonas Götz <jonas@42heilbronn.de>
2025-05-26 13:34:43 +0000

    get the users from raw queries

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,30 +1,32 @@
-import { Controller, Get, UseGuards, Response, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, UseGuards, Response, Param, ParseIntPipe, Logger } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from '../entities/user.entity';
 import { JwtAuthGuard } from '../auth/jwt.auth.guard';
 import { ApiParam } from '@nestjs/swagger';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller()
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService, private readonly authService: AuthService) { }
 
   @Get("users")
   @UseGuards(JwtAuthGuard)
-  getAllUsers(): Promise<User[]> {
-    return this.usersService.findAll();
+  async getAllUsers(): Promise<any> {
+    return await this.usersService.dataSource.query(`SELECT * FROM auth.users`)
   }
 
   @Get("users/:id")
-  @ApiParam({ name: 'id', required: true, description: 'integer for the id of the user', schema: { type: 'string' } })
+  @ApiParam({ name: 'id', required: true, description: 'ID of the user', schema: { type: 'string' } })
   @UseGuards(JwtAuthGuard)
-  async getUserByEmail(@Response() res: any, @Param('id') id: string): Promise<any> {
-    return this.usersService.findOne(id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
-        }
-        return res.json(user);
+  async getUserById(@Param('id') id: string, @Response() res: any): Promise<any> {
+    (await this.usersService.dataSource.query(`SELECT * FROM auth.users WHERE id = $1`, [id]).then((user) => {
+      if (user.length === 0) {
+        res.status(404).json({ error: 'User not found' });
+        return;
       }
-      )
+      res.status(200).json(user[0]);
+    })
+      .catch((err) => {
+        res.status(404).json({ error: 'User not found' });
+      }));
   }
 }
