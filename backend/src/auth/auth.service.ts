498a721368c2f677fd1330f76dcdf1aa5a0a1f43
Jonas Götz <jonas@42heilbronn.de>
2025-05-26 13:33:10 +0000

    style

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,5 +6,5 @@ export class AuthService {
     supabaseUrl = "http://kong:8000",
     supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q",
     public supabase = createClient(supabaseUrl, supabaseKey)
-  ) {}
+  ) { }
 }
